820d33c930fecb01f0b1f3ae78c064b7503d978f
yurich-official <PC.yurich@gmail.com>
2024-12-29 14:57:04 +0900

    improved mechanism of adding commands

---
+++ commands_list.py
@@ -0,0 +1,5 @@
+import commands.help as help
+import commands.history as history
+import commands.manual as manual
+import commands.new as new
+import commands.view as view
--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import telebot
 import command_manager as cm
-import commands.help as help
-import commands.manual as manual
-import commands.new as new
-import commands.view as view
-import commands.history as history
+import commands_list
+import aliases_list
 import json
 import database
 
@@ -18,12 +15,19 @@ bot = telebot.TeleBot(TOKEN)
 db = database.UserDatabase("users.json")
 
 command_manager = cm.CommandManager(bot, db)
-command_manager.add_command("help", help.callback)
-command_manager.add_alias("help", "start")
-command_manager.add_command("manual", manual.callback)
-command_manager.add_command("new", new.callback)
-command_manager.add_command("view", view.callback)
-command_manager.add_command("history", history.callback)
+for i in dir(commands_list):
+    print(i)
+    if i.startswith("__"):
+        continue
+    module = getattr(commands_list, i)
+    command_manager.add_command(i, module.callback)
+for i in dir(aliases_list):
+    print(i)
+    if i.startswith("__"):
+        continue
+    val = getattr(aliases_list, i)
+    command_manager.add_alias(val, i)
+print(command_manager.commands)
 command_manager.listen()
 
 bot.infinity_polling()
