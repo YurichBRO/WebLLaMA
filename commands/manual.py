4e51041f861019f5186be83b426417e39feaf7ba
yurich-official <PC.yurich@gmail.com>
2024-12-29 12:14:26 +0900

    bot now has manual onn how to create one

---
+++ commands/manual.py
@@ -0,0 +1,11 @@
+import logging
+
+try:
+    with open("commands/manual-message.txt", encoding="utf-8") as manual_message_file:
+        manual_message = manual_message_file.read()
+except:
+    logging.critical("Failed to open manual-message.txt")
+    exit(1)
+else:
+    def callback(bot, message):
+        bot.send_message(message.from_user.id, manual_message)
--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import telebot
 import command_manager as cm
 import commands.help as help
+import commands.manual as manual
+import json
 
-TOKEN = "7627113998:AAGd0gcNvdi3FfJfsdNh7sYoNT7YWaSR55c"
+with open("config.json") as config:
+    config_data = json.load(config)
+    TOKEN = config_data["token"]
+    
 OLLAMA_MODEL = "llama3.2"
 
 bot = telebot.TeleBot(TOKEN)
+
 command_manager = cm.CommandManager(bot)
 command_manager.add_command("help", help.callback)
 command_manager.add_alias("help", "start")
-
+command_manager.add_command("manual", manual.callback)
 command_manager.listen()
+
 bot.infinity_polling()
