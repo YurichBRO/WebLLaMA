0014894aa05caca5797b1cc3eceebc42d6e9cb5d
yurich-official <PC.yurich@gmail.com>
2024-12-29 21:49:51 +0900

    fixed a bug in view.py in truncate function (now passing the correct thing in it)

--- commands/view.py
+++ commands/view.py
@@ -6,6 +6,6 @@ def callback(bot, db, message):
     result = [f"Чат №{db.get_current_chat_number(from_)}"]
     for i in chat:
         role = i['role'].ljust(9)
-        text = truncate(i.text, 80)
+        text = truncate(i['content'], 80)
         result.append(f'{role}: {text}')
     bot.send_message(from_, '\n'.join(result))
